36397e0ca78a0c598f20d9bf587ad614f744170f
Maksim Stefanovich <maxim.stefanovich85@gmail.com>
2022-05-29 15:16:30 +0200

    hw-4

--- JS_Algoritms/script.js
+++ JS_Algoritms/script.js
@@ -1,9 +1,42 @@
 // Известно, что X кг конфет стоит A рублей. Определить, сколько стоит 1 кг и Y кг этих же конфет.
 
 {
+function oneKg(a , x){
+const oneKg = a / x;
+return costOneKg;
+}
 
-  const oneKg = 100
-  const Y = 30
-  console.log(oneKg * Y)
+function task1a(a, x, y)
+{
+  oneKg = oneKg(a , x);
+  let yKg = oneKg * y:
+}
 
+function task1b(a, b, x, y)
+{
+  oneKgA = oneKg(a , x);
+  oneKgB = oneKg(b , y);
+ return let dif = oneKgA / oneKgB;
+}
+
+function triangle( a, b, c)
+{
+ if(a == b || a == c || b == c ) return true;
+ return false;
+}
+
+function distance(a, b, c)
+{
+if ((c — a) < (b — a))
+ abs(c — a);
+else
+ abs(b — a);
+}
+function sFact(n)
+{
+    var rval =1;
+    for (var i = 2; i <= n; i++)
+        rval = rval * i;
+    return rval;
+}
 }
